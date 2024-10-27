f9d420eaf7e0fa509a9be67aa8387b3156213161
Nathan Ford <nfor6010@uni.sydney.edu.au>
2024-10-28 10:42:21 +1100

    BUild login page

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,8 +1,87 @@
-import React from "react";
-
+import React, { useState } from "react";
+import {useNavigate} from 'react-router-dom';
 
 const LoginPage = () => {
-    return <h1>Welcome to the Login page of the E-commerce App</h1>
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // Handle form susmission (API call will go here)
+        try {
+            const response = await fetch('http://localhost:5000/api/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+
+            const data = await response.json();
+            if (response.ok) {
+                // Registration successful, redirect or auto-login
+                console.log('Login successful:', data);
+                navigate('/products');
+                // Redirect to another page e.g. products
+            } else {
+                console.error('Login failed:', data.message);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                     type="email"
+                     id="email"
+                     name="email"
+                     value={formData.email}
+                     onChange={handleChange}
+                     required
+                     />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                     type="password"
+                     id="password"
+                     name="password"
+                     value={formData.password}
+                     onChange={handleChange}
+                     required
+                     />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+
+            <div>
+                <p>Or login with:</p>
+                <button>Login with Google</button>
+                <button>Login with Facebook</button>
+            </div>
+
+            <p>
+                Don't have an account? <a href="/register">Register here</a>
+            </p>
+        </div>
+    );
 };
 
 export default LoginPage;
