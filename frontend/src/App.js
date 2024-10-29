7a044c33dca98c353436a3986eec38aa166227cb
Nathan Ford <nfor6010@uni.sydney.edu.au>
2024-10-29 15:48:11 +1100

    wrap app in stripe elements

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,10 @@ import LoginPage from './components/LoginPage';
 import NavBar from './NavBar';
 import ProductDetails from './components/ProductDetails';
 import CartPage from './components/CartPage';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe('YOUR_PUBLISHABLE_KEY');
 
 function App() {
 
@@ -32,19 +36,21 @@ function App() {
 
 
   return (
-    <Router>
-      <NavBar onLogout={handleLogout} />
-      <div className='App'>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/products' element={<ProductsPage />} />
-          <Route path='/register' element={<RegisterPage />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/products/:id' element={<ProductDetails />} />
-          <Route path='/cart' element={<CartPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <Elements stripe={stripePromise}>
+      <Router>
+        <NavBar onLogout={handleLogout} />
+        <div className='App'>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/products' element={<ProductsPage />} />
+            <Route path='/register' element={<RegisterPage />} />
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/products/:id' element={<ProductDetails />} />
+            <Route path='/cart' element={<CartPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </Elements>
   );
 }
 
