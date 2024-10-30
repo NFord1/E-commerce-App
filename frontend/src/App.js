6410f5f6090d6d826f1102a0024d0c0522809ed2
Nathan Ford <nfor6010@uni.sydney.edu.au>
2024-10-30 21:45:35 +1100

    Add login status check to toggle login/ logout button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import ProductsPage from './components/ProductsPage';
@@ -16,6 +16,31 @@ import { loadStripe } from '@stripe/stripe-js';
 const stripePromise = loadStripe('REMOVED_API_KEY');
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Check login status on initial load
+    const checkLoginStatus = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/api/auth/status', {
+          method: 'GET',
+          credentials: 'include',
+        });
+
+        if (response.ok) {
+          setIsLoggedIn(true);
+        } else {
+          setIsLoggedIn(false);
+        }
+        //setIsLoggedIn(response.ok); // If response is OK, user is logged in
+      } catch (error) {
+        console.error('Error checking login status:', error);
+      }
+    };
+
+    checkLoginStatus();
+  }, []);
+
 
   const handleLogout = async () => {
     try {
@@ -26,6 +51,7 @@ function App() {
 
         if (response.ok) {
             console.log('Logout successful');
+            setIsLoggedIn(false);
             window.location.href = 'http://localhost:3000';
         } else {
             console.error('Logout failed');
@@ -39,13 +65,13 @@ function App() {
   return (
     <Elements stripe={stripePromise}>
       <Router>
-        <NavBar onLogout={handleLogout} />
+        <NavBar isLoggedIn={isLoggedIn} onLogout={handleLogout} />
         <div className='App'>
           <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/products' element={<ProductsPage />} />
             <Route path='/register' element={<RegisterPage />} />
-            <Route path='/login' element={<LoginPage />} />
+            <Route path='/login' element={<LoginPage onLogin={() => setIsLoggedIn(true)}/>} />
             <Route path='/products/:id' element={<ProductDetails />} />
             <Route path='/cart' element={<CartPage />} />
             <Route path='/orders/history' element={<OrderHistoryPage />} />
