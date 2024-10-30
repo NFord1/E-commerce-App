dc6cd8cef7bc41cd58c52d9ab2e091ee2fd9bb06
Nathan Ford <nfor6010@uni.sydney.edu.au>
2024-10-30 21:44:44 +1100

    Create navbar component

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import './NavBar.css';
 
-const NavBar = ({ onLogout }) => {
+
+const NavBar = ({ isLoggedIn, onLogout }) => {
     return (
-        <nav>
-            <h1>Ecommerce Website Project</h1>
-            <button onClick={onLogout}>Logout</button>
+        <nav className="navbar">
+            <h1 className="navbar-title">Ecommerce Website Project</h1>
+            <div className="navbar-links">
+                <Link to="/" className="nav-link">Home</Link>
+                <Link to="/products" className="nav-link">Products</Link>
+                <Link to="/cart" className="nav-link">Cart</Link>
+                {isLoggedIn ? (
+                    <button onClick={onLogout} className="nav-button">Logout</button>
+                ) : (
+                    <Link to="/login" className="nav-link">Login</Link>
+                )}
+            </div>
         </nav>
     );
 };
